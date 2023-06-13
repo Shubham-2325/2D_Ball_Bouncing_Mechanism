965f7e850dd08ceae0b46fbebb65ebd5623b95f4
Shubham-2325 <shubhamsharma2325@gmail.com>
2023-06-14 01:17:17 +0530

    Initial Value input code added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+using namespace std;
+
+void ball_initial_pos(int &x, int &y, int &theta_angle)
+{
+
+    cout << "please give intial x-position for the ball:\n";
+    cin >> x;
+    cout << "please give intial y-position for the ball:\n";
+    cin >> y;
+    cout << "please give intial angle with the origin for the ball:\n";
+    cin >> theta_angle;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int x{};
+    int y{};
+    int theta_angle{};
+
+    ball_initial_pos(x,y,theta_angle);
+
+    //motor_movement_function()
+
     return 0;
+
 }
